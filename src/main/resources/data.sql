6436986a334f6ceaa55b8d883ea79a6a071aec2f
nishkashyap <100674628+nishkashyap@users.noreply.github.com>
2022-03-21 13:47:01 +0530

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into EMPLOYEE values (1,'MNO','B7','SDE1','MNO@rty.com','12345')
-insert into EMPLOYEE values (2,'ABC','B7','SDE1','ABC@rty.com','67890')
-insert into EMPLOYEE values (3,'XYZ','B7','SDE1','XYZ@rty.com','09876')
-insert into EMPLOYEE values (4,'POR','B7','SDE1','PQR@rty.com','54321')
